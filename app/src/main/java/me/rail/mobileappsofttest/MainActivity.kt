e2a5eeb93b16160e31dde829fab0eeea2a263211
Мещеров Раиль Наильевич <meshcherov.rail@itmh.ru>
2021-10-29 20:43:41 +0300

    add edittext

--- app/src/main/java/me/rail/mobileappsofttest/MainActivity.kt
+++ app/src/main/java/me/rail/mobileappsofttest/MainActivity.kt
@@ -1,7 +1,11 @@
 package me.rail.mobileappsofttest
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.WindowManager
+import android.view.inputmethod.InputMethodManager
 import androidx.databinding.DataBindingUtil
 import me.rail.mobileappsofttest.databinding.ActivityMainBinding
 
@@ -12,5 +16,13 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
 
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        binding?.add?.setOnClickListener {
+            binding?.edittext?.visibility = View.VISIBLE
+            binding?.edittext?.requestFocus()
+            val imm: InputMethodManager =
+                getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+            imm.showSoftInput(binding?.edittext, InputMethodManager.SHOW_IMPLICIT)
+        }
     }
 }
