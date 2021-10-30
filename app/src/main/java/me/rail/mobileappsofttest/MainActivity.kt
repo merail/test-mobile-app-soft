3e056724f1500b9afe8bbb1b7ff6113cbef71933
Мещеров Раиль Наильевич <meshcherov.rail@itmh.ru>
2021-10-30 23:48:50 +0300

    change background color shen note create

--- app/src/main/java/me/rail/mobileappsofttest/MainActivity.kt
+++ app/src/main/java/me/rail/mobileappsofttest/MainActivity.kt
@@ -7,6 +7,7 @@ import android.view.inputmethod.InputMethodManager
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
 import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.MutableLiveData
 import me.rail.mobileappsofttest.databinding.ActivityMainBinding
 
 
@@ -21,15 +22,34 @@ class MainActivity : AppCompatActivity() {
         binding?.add?.setOnClickListener {
             binding?.edittext?.visibility = View.VISIBLE
             binding?.edittext?.requestFocus()
+            binding?.edittext?.setListener(getOnKeyboardBackPressedListener())
             val imm: InputMethodManager =
                 getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
             imm.showSoftInput(binding?.edittext, InputMethodManager.SHOW_IMPLICIT)
-            binding?.main?.setBackgroundColor(
-                ContextCompat.getColor(
-                    applicationContext,
-                    R.color.blur
-                )
-            )
         }
+    }
+
+    private fun getOnKeyboardBackPressedListener(): MutableLiveData<Boolean> {
+        val onKeyboardBackPressed = MutableLiveData<Boolean>()
+        onKeyboardBackPressed.value = false
+
+        onKeyboardBackPressed.observeForever {
+            if (it)
+                binding?.main?.setBackgroundColor(
+                    ContextCompat.getColor(
+                        applicationContext,
+                        R.color.white
+                    )
+                )
+            else
+                binding?.main?.setBackgroundColor(
+                    ContextCompat.getColor(
+                        applicationContext,
+                        R.color.blur
+                    )
+                )
+        }
+
+        return onKeyboardBackPressed
     }
 }
