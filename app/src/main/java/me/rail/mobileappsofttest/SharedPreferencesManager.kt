dac072516c2edd467e64646748f4a4484250f306
Мещеров Раиль Наильевич <meshcherov.rail@itmh.ru>
2021-10-31 16:12:14 +0300

    add room dependencies

--- app/src/main/java/me/rail/mobileappsofttest/SharedPreferencesManager.kt
+++ app/src/main/java/me/rail/mobileappsofttest/SharedPreferencesManager.kt
@@ -1,24 +1,22 @@
 package me.rail.mobileappsofttest
 
 import android.content.Context
-import com.google.gson.Gson
-import com.google.gson.reflect.TypeToken
 
 class SharedPreferencesManager(context: Context) {
-    private val sharedPreferences =
-        context.getSharedPreferences(PREFERENCES_NAME, Context.MODE_PRIVATE)
-    private val editor = sharedPreferences.edit()
-    private val noteArrayListType = object : TypeToken<ArrayList<Note>?>() {}.type
-
-    var notes: ArrayList<String>?
-        get() = Gson().fromJson(sharedPreferences.getString(NOTES_KEY, ""), noteArrayListType)
-        set(value) = editor.putString(
-            NOTES_KEY, Gson().toJson(value)
-        ).apply()
-
-    companion object {
-        private const val PREFERENCES_NAME = "Mobile Soft App Test Preferences"
-
-        private const val NOTES_KEY = "Notes Key"
-    }
+//    private val sharedPreferences =
+//        context.getSharedPreferences(PREFERENCES_NAME, Context.MODE_PRIVATE)
+//    private val editor = sharedPreferences.edit()
+//    private val noteArrayListType = object : TypeToken<ArrayList<Note>?>() {}.type
+//
+//    var notes: ArrayList<String>?
+//        get() = Gson().fromJson(sharedPreferences.getString(NOTES_KEY, ""), noteArrayListType)
+//        set(value) = editor.putString(
+//            NOTES_KEY, Gson().toJson(value)
+//        ).apply()
+//
+//    companion object {
+//        private const val PREFERENCES_NAME = "Mobile Soft App Test Preferences"
+//
+//        private const val NOTES_KEY = "Notes Key"
+//    }
 }
